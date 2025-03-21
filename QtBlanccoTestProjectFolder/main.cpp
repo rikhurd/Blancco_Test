0b2577e4f0744ca80ebfede426030913cbe47e1c
Riku Härkönen <riku.harkonen1@gmail.com>
2025-03-21 20:18:50 +0100

    Add debug for QQuickView object
    Remove functionality not being able to launch the app without getting a QML file from the server. Like the comment says: "but to be optimal and readable should be made into a debug line for the user."

--- QtBlanccoTestProjectFolder/main.cpp
+++ QtBlanccoTestProjectFolder/main.cpp
@@ -21,20 +21,22 @@ int main(int argc, char *argv[])
 
     NetworkManager* networkManager = new NetworkManager;
 
-    QObject::connect(networkManager, &NetworkManager::postRequestFinished,
-                     [=](const QUrl &url) {
-                         if (url.isValid()) {
-                            view->setSource(url);
-                             // Debug URL differs from one got in server, because we do a temporary file of it.
-                            qDebug() << "URL received: " << url.toString();
-                         } else {
-                             qDebug() << "Invalid URL received: " << url.toString();
-                         }
-                     });
+    QObject::connect(networkManager, &NetworkManager::postRequestFinished,[=](const QUrl &url) {
+        if (url.isValid()) {view->setSource(url);
+            // Debug URL differs from one got in server, because we do a temporary file of it.
+            qDebug() << "URL received: " << url.toString();
+
+            /*
+             *  We call view after the URL is confirmed to be received. Not optimal but circumvents window starting without getting a file from the server.
+             *  Done so we can't launch the app without a server, but to be optimal and readable should be made into a debug line for the user.
+             */
+            view->show();
+        } else {
+            qDebug() << "Invalid URL received: " << url.toString();
+        }
+    });
 
     networkManager->SendPostRequest("http://localhost:8000/api/v1/getQML", "Main.qml");
-
-    view->show();
 
     return app.exec();
 }
